63578c0a6f11ca666dfdf1e53524a484436ebc28
rakel25 <raquelblisa@gmail.com>
2025-06-11 19:20:32 +0100

    Add files via upload
    Actualización del probador virtual con camisetas corregidas

---
+++ script.js
@@ -0,0 +1,67 @@
+const videoElement = document.getElementById('video');
+const canvasElement = document.getElementById('canvas');
+const canvasCtx = canvasElement.getContext('2d');
+
+const camisetas = [
+  new Image(),
+  new Image(),
+  new Image(),
+  new Image()
+];
+
+camisetas[0].src = 'camisetas/azul.png';
+camisetas[1].src = 'camisetas/morada.png';
+camisetas[2].src = 'camisetas/naranja.png';
+camisetas[3].src = 'camisetas/verde.png';
+
+let camisetaActual = 0;
+
+function cambiarCamiseta(index) {
+  camisetaActual = index;
+}
+
+const pose = new Pose.Pose({
+  locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`
+});
+
+pose.setOptions({
+  modelComplexity: 1,
+  smoothLandmarks: true,
+  enableSegmentation: false,
+  minDetectionConfidence: 0.5,
+  minTrackingConfidence: 0.5
+});
+
+pose.onResults(onResults);
+
+const camera = new Camera(videoElement, {
+  onFrame: async () => {
+    await pose.send({image: videoElement});
+  },
+  width: 640,
+  height: 480
+});
+camera.start();
+
+function onResults(results) {
+  canvasElement.width = videoElement.videoWidth;
+  canvasElement.height = videoElement.videoHeight;
+
+  canvasCtx.save();
+  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+  canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
+
+  if (results.poseLandmarks) {
+    const leftShoulder = results.poseLandmarks[11];
+    const rightShoulder = results.poseLandmarks[12];
+
+    const x = leftShoulder.x * canvasElement.width;
+    const y = leftShoulder.y * canvasElement.height;
+    const width = (rightShoulder.x - leftShoulder.x) * canvasElement.width;
+    const height = width * 1.2;
+
+    canvasCtx.drawImage(camisetas[camisetaActual], x, y, width, height);
+  }
+
+  canvasCtx.restore();
+}
